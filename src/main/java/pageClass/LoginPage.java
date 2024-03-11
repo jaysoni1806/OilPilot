3372ad9d482df84da61507abdb9aaf581b6dddec
Jay Soni <89600182+jaysoni1806@users.noreply.github.com>
2024-03-11 11:54:43 +0530

    Xpath Caeg

--- src/main/java/pageClass/LoginPage.java
+++ src/main/java/pageClass/LoginPage.java
@@ -26,7 +26,7 @@ public class LoginPage {
 	public WebElement email;
 	@FindBy(xpath = "//input[@type='password']")
 	public WebElement pass;
-	@FindBy(xpath = "//button[@type='submit']")
+	@FindBy(xpath = "//button[@type='submit1']")
 	public WebElement sbtBtn;
 	@FindBy(xpath = "//div[contains(@class,'css-acwcvw')]//p[contains(@id,'helper-text')]")
 	public List<WebElement> emailError;
